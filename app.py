864e691df3a8bb53239c4bbfadfb227b0c516d54
Kadir545454 <tektumura@gmail.com>
2025-10-30 13:26:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ HTML ="""
 <!doctype html>
 <html>
 <head>
-<title>istanbul tripten Selam</title>
+<title>ADAPAZAR 54</title>
 <style>
     body {font-family: Arial; text-align: center; padding: 50px; background: #eef2f3;}
     h1 { color: #333; }
